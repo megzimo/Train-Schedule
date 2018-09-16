39ee5c1fe858afde568042600860c3ec9b91425c
megzimo <40956115+megzimo@users.noreply.github.com>
2018-09-16 14:59:43 -0500

    all working except first train time display in unix

--- assets/js/train.js
+++ assets/js/train.js
@@ -1,62 +1,65 @@
-$(document).ready(function() {
+$(function () {
+    let database = firebase.database();
+    //sets a variable to the firebase database so that we don't have to keep retyping it
+    let dataRef = database.ref("/trains");
 
-let train = [];
-let dest = [];
-let firstTrain = [];
-let freq = [];
-
-  // target each input and store as a variable
-  function addTrain() {
-    let newTrain = $("#name").val();
-    console.log("recent train added: ", newTrain);
-    if (newTrain.length === 0) {
-      alert("Please provide train name.");
-      return false;
-    }
-    train.push(newTrain);
-    console.log("train array: ", train);
-  } // ENDS addTrain()
-
-  function addDestination() {
-    let newDest = $("#destination").val();
-    if (newDest.length === 0) {
-      alert("Please provide train destination.");
-      return false;
-    }
-    dest.push(newDest);
-    console.log("destination array: ", dest);
-  } // ENDS addDestination()
-
-  function addFirstTrain() {
-    let newFirst = $("#train-time").val();
-    if (newFirst.length === 0) {
-      alert("Please provide a time for the first train arrival.");
-      return false;
-    }
-    firstTrain.push(newFirst);
-    console.log("first train array: ", firstTrain);
-} // ENDS addFirstTrain()
-
-  function addTrainFreq() {
-    let newFreq = $("#freq").val();
-    if (newFreq.length === 0) {
-      alert("Please provide train frequency.");
-      return false;
-    }
-    freq.push(newFreq);
-    console.log("freq array: ", freq);
-} // ENDS addTrainFreq()
-
-  // Function that runs all functions for new input at once
-  $(".btn").on("click", function addInfo(e) {
+    
+    $("#submit").on('click', function(e) {
         e.preventDefault();
-        console.log("click event: ", e);
-        addTrain();
-        addDestination();
-        addFirstTrain();
-        addTrainFreq();
 
-  }); // ENDS addInfo()
+        let name = $("#train-name").val().trim();
+        let dest = $("#destination").val().trim();
+        let first = moment($("#first-train").val().trim(), "HH:mm").subtract(1, "years").format("X");
+        console.log("first train type: ", typeof first)
+        let freq = $("#frequency").val().trim();
 
-  //push the variable to the bottom of the list area
-}); // ENDS doc ready
+
+        // submit the input values to the database
+        database.ref("/trains").push({
+            name: name,
+            dest: dest,
+            first: first,
+            freq: freq
+        })
+
+        $("#train-name").val("");
+        $("#destination").val("");
+        $("#first-train").val("");
+        $("#frequency").val("") ;      
+        
+        return false;        
+    });
+
+    dataRef.on("child_added", function(snapshot){
+        let trainInfo = snapshot.val();
+
+        let tName = trainInfo.name;
+        let tDest = trainInfo.dest;
+        let tFirst = trainInfo.first;
+        let tFreq = trainInfo.freq;
+
+        // values need to be dynamically added after calculation with moment
+       let tRemaining = moment().diff(moment.unix(tFirst), "minutes") % tFreq;
+       let tMin = tFreq - tRemaining;
+
+       let tNext = moment().add(tMin, "m").format("hh:mm A");
+       console.log("tnext: ", tNext)
+
+
+        $("#train-sched").append("<tr><td>" + tName + "</td><td>" + tDest + "</td><td>" + tFreq + "</td><td>" + tFirst + "</td><td>" + tNext + "</td><td>" + tMin + "</td></tr>");
+
+    })
+
+
+}); // ENDS initial function
+
+
+
+// Calculate the minutes until arrival using hardcore math
+// To calculate the minutes till arrival, take the current time in unix subtract the FirstTrain time
+// and find the modulus between the difference and the frequency.
+// var differenceTimes = moment().diff(trainTime, "minutes");
+// var tRemainder = differenceTimes % tFrequency;
+// tMinutes = tFrequency - tRemainder;
+// To calculate the arrival time, add the tMinutes to the current time
+// tArrival = moment().add(tMinutes, "m").format("hh:mm A");
